1163e20e23e32a05d768d1a2d6db4fa9595d2464
Muhammad Hammad <muhammadhammad20004@gmail.com>
2025-09-11 20:03:21 +0800

    Frontend Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-
 import Sidebar from './components/Sidebar'
 import Chatbox from './components/Chatbox'
 import Credits from './pages/Credits'
@@ -8,28 +7,43 @@ import { useState } from 'react'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import Login from './pages/Login'
+import { useAppContext } from './context/Appcontext'
+
 const App = () => {
-
+  const { user } = useAppContext()
   const [isOpenMenu, setIsOpenMenu] = useState(false)
-
   const { pathname } = useLocation()
 
-  if(pathname === '/loading')  return <Loading />
+  if (pathname === '/loading') return <Loading />
 
   return (
     <>
-    { !isOpenMenu && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={() => setIsOpenMenu(true)} alt="" /> }
-    <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
-     <div className='flex h-screen w-screen'>
-       <Sidebar isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
-       <Routes>
-          <Route path='/' element={ <Chatbox /> } />          
-          <Route path='/credits' element={ <Credits /> } />          
-          <Route path='/community' element={ <Community /> } />          
-       </Routes>
+      {!isOpenMenu && (
+        <img
+          src={assets.menu_icon}
+          className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+          onClick={() => setIsOpenMenu(true)}
+          alt=""
+        />
+      )}
+
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        {user ? (
+          <div className="flex h-screen w-screen">
+            <Sidebar isOpenMenu={isOpenMenu} setIsOpenMenu={setIsOpenMenu} />
+            <Routes>
+              <Route path="/" element={<Chatbox />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </div>
+        ) : (
+          <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+            <Login />
+          </div>
+        )}
       </div>
-    </div>
-    
     </>
   )
 }
